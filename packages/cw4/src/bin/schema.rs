ecb3f987356716f3a39f6fc5b46fa858193273d1
Kayanski <kowalski.kowalskin@gmail.com>
2024-03-20 16:25:17 +0000

    Udpated cw-orch

--- packages/cw4/src/bin/schema.rs
+++ packages/cw4/src/bin/schema.rs
@@ -4,7 +4,7 @@ use std::fs::create_dir_all;
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
 use abstract_cw4::{
-    AdminResponse, Cw4ExecuteMsg, Cw4QueryMsg, Member, MemberChangedHookMsg, MemberListResponse,
+    AdminResponse, Cw4ExecuteMsg, Cw4QueryMsg, MemberChangedHookMsg, MemberListResponse,
     MemberResponse, TotalWeightResponse,
 };
 
