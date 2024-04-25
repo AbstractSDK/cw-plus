c72156f87c807997acd07e2cc1462217964ca700
Kayanski <kowalski.kowalskin@gmail.com>
2024-04-25 12:00:19 +0000

    Fix publish

--- publish.sh
+++ publish.sh
@@ -6,21 +6,21 @@ PACKAGES="
 "
 CONTRACTS="cw1-whitelist cw1-subkeys cw20-base cw20-ics20 cw4-stake cw4-group cw3-fixed-multisig cw3-flex-multisig"
 
-# for pack in $PACKAGES; do
-#   (
-#     cd "packages/$pack"
-#     echo "Publishing $pack"
-#     cargo publish
-#   )
-# done
+for pack in $PACKAGES; do
+  (
+    cd "packages/$pack"
+    echo "Publishing $pack"
+    cargo publish
+  )
+done
 
-# for lib in $CONTRACTS; do
-#   (
-#     cd "contracts/$lib"
-#     echo "Publishing $lib"
-#     cargo publish
-#   )
-# done
+for lib in $CONTRACTS; do
+  (
+    cd "contracts/$lib"
+    echo "Publishing $lib"
+    cargo publish
+  )
+done
 
 
 (
